f0ecd46d0993b6c1d49fd603d2d6c80faf915d4f
nguyenquynh201 <nguyenquynhqb98@gmail.com>
2023-01-05 14:09:41 +0700

    feat : config package name

--- app/screen/HomeScreen.js
+++ app/screen/HomeScreen.js
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { Text, View } from 'react-native';
 import React, { Component } from 'react';
-
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Styles } from "@/constants"
 export class HomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>HomeScreen</Text>
-      </View>
+      <SafeAreaView>
+
+      </SafeAreaView>
     );
   }
 };
