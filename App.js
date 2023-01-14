f13ff22a4eb83b0862b9adce59ffc3e871a60147
Hades <phamvanlinh.hdes.1711@gmail.com>
2023-01-14 17:04:31 +0700

    UI_upcoming

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {MovieDetail} from './screens';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Upcoming">
+        <Stack.Screen name="Upcoming" component={Tabs} />
+        <Stack.Screen name="MovieDetail" component={MovieDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
