bcac4d3feef1d5a17629cfa6966447fcb33e5404
anhduoc0935 <91368497+anhduoc0935@users.noreply.github.com>
2023-01-16 22:01:47 +0700

    up seeall

--- index.js
+++ index.js
@@ -5,7 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import ResultList from '@/screen/Result/ResultList';
-import HomeScreen from '@/screen/home/HomeScreen';
-import MovieDetail from '@/screen/detail/home_detail/MovieDetail';
-AppRegistry.registerComponent(appName, () => ResultList);
+
+AppRegistry.registerComponent(appName, () => App);
