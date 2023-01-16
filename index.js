2a505587a81b1f86efdc0184e68a88601637dfb9
nguyenquynh201 <nguyenquynhqb98@gmail.com>
2023-01-16 21:42:43 +0700

    fix

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import ResultList from '@/screen/Result/ResultList';
 import HomeScreen from '@/screen/home/HomeScreen';
 import MovieDetail from '@/screen/detail/home_detail/MovieDetail';
-AppRegistry.registerComponent(appName, () => ResultList);
+AppRegistry.registerComponent(appName, () => App);
